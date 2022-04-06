f026b84941d0335da0fe0094933df185845c7c8e
llmanoff <102532503+llmanoff@users.noreply.github.com>
2022-04-06 15:40:35 +0300

    Add files via upload

---
+++ Mathematics1.java
@@ -0,0 +1,8 @@
+package math;
+
+public class Mathematics1 {
+    public static void main(String[] args) {
+                String myName = "Lachezar";
+                System.out.println("8\t9");
+    }
+}
